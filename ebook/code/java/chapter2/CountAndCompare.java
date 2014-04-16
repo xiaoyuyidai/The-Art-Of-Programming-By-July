e7eb4421f6499923107532b4a0ab997448c8b27d
xiaoyuyidai <xiaoyuyidai@126.com>
2014-04-16 20:53:11 +0800

    新增用素数方案isExistsBySushu
    使用BigInteger来解决Int乘积超过最大值情况

--- ebook/code/java/chapter2/CountAndCompare.java
+++ ebook/code/java/chapter2/CountAndCompare.java
@@ -1,4 +1,19 @@
 class CountAndCompare implements ICompare {
+    
+    private static String a = "ABCDMNOPQRSABCDEFGHL";
+	
+	private static String b = "DCGSRQPOt";
+	
+	private static char[] aChar;
+	
+	private static char[] bChar;
+	
+	static{
+		aChar = a.toUpperCase().toCharArray();
+		bChar = b.toUpperCase().toCharArray();
+	}
+	
+	
     @Override
     public boolean compare(String longString, String shortString) {
         int posLong = 0, posShort = 0;
@@ -32,6 +47,29 @@ class CountAndCompare implements ICompare {
         }
         return buf.toString();
     }
+    
+    //牛逼大招 用素数方案！
+    public static boolean isExistsBySushu(){
+		int p[] = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,61, 67, 71, 73, 79, 83, 89, 97, 101};
+		BigInteger count = new BigInteger("1");
+		
+		for(int i=0;i<aChar.length;i++){
+			BigInteger temp = new BigInteger(String.valueOf(p[aChar[i]-'A']));
+			count = count.multiply(temp);
+		}
+		System.out.println(count);
+		for(int i=0;i<bChar.length;i++){
+			BigInteger temp = new BigInteger(String.valueOf(p[bChar[i]-'A']));
+			if(count.remainder(temp).equals("0")){
+				return false;
+			}
+		}
+		return true;
+	}
+	 
+	public static void main(String args[]){
+		System.out.println(isExistsBySushu());
+	}
 
 
 }
